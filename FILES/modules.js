e6b771afa80f4549dd8e4042188a4a787b70aa62
Npnbit <collinsmutua92@gmail.com>
2025-09-27 07:43:14 +0000

    functions

---
+++ FILES/modules.js
@@ -0,0 +1,19 @@
+//This is my call function 
+function sum(a, b){
+let result = a * b;
+return result;
+}
+
+// i'm declaring my call function
+let result = sum(4, 3);
+console.log(result);
+
+//This is my call function 
+function sums(a, b){
+let results = a + b;
+return results;
+}
+
+// i'm declaring my call function
+let results = sums(4, 3)
+console.log(results);
